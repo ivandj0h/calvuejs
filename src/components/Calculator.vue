ea3d670e8ed8846afbbb991b521bf2f502996723
ivandi1980 <ivandi.djoh@gmail.com>
2019-10-16 15:09:42 +0800

    replace all default vue boilerplate based on our own needs

---
+++ src/components/Calculator.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="calculator">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
